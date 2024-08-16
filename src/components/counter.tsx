3ed1bcc805a8179f006a1006c72ba879b9733b09
webgeny <evgenyvolovlikov@gmail.com>
2024-08-16 12:27:02 +0300

    add react & scss support, add test component

---
+++ src/components/counter.tsx
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+import './counter.scss';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    return (
+        <div className='counter'>
+            <h1>{count}</h1>
+            <button onClick={increment}>increment</button>
+        </div>
+    );
+};
+
+export default Counter;
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function someFunc(str: string) {
-	console.log('string')
-	return str
-}
