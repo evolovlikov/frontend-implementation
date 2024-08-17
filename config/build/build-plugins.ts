25ded71e03ba4ef03784973e4a7845ae82187017
webgeny <evgenyvolovlikov@gmail.com>
2024-08-17 12:28:22 +0300

    add bundle-analyzer support

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -1,5 +1,6 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import {ProgressPlugin, DefinePlugin, HotModuleReplacementPlugin} from 'webpack';
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import {BuildOptions} from './types/config';
 
 export function buildPlugins({paths, isDev}: BuildOptions) {
@@ -12,5 +13,9 @@ export function buildPlugins({paths, isDev}: BuildOptions) {
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
 		new HotModuleReplacementPlugin(),
+		// Анализ бандла
+		new BundleAnalyzerPlugin({
+			openAnalyzer: false,
+		}),
 	];
 }
