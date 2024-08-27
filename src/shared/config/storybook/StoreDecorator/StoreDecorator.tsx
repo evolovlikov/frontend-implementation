7ca03ad49c8895f41185c617c1529a9ecaea462e
webgeny <evgenyvolovlikov@gmail.com>
2024-08-27 18:18:33 +0300

    add class for async tests, refactor with prettier

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,17 +1,17 @@
-import { Story } from '@storybook/react';
-import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
-import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+import {Story} from '@storybook/react';
+import {StateSchema, StoreProvider} from 'app/providers/StoreProvider';
+import {DeepPartial, ReducersMapObject} from '@reduxjs/toolkit';
+import {loginReducer} from 'features/AuthByUsername/model/slice/loginSlice';
 
 const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
-    loginForm: loginReducer,
+	loginForm: loginReducer,
 };
 
-export const StoreDecorator = (
-    state: DeepPartial<StateSchema>,
-    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
-) => (StoryComponent: Story) => (
-    <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
-        <StoryComponent />
-    </StoreProvider>
-);
+export const StoreDecorator =
+	(state: DeepPartial<StateSchema>, asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>) =>
+	(StoryComponent: Story) =>
+		(
+			<StoreProvider initialState={state} asyncReducers={{...defaultAsyncReducers, ...asyncReducers}}>
+				<StoryComponent />
+			</StoreProvider>
+		);
