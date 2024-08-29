5064303c67a83c978ea322acd4103b6ecde05540
webgeny <evgenyvolovlikov@gmail.com>
2024-08-29 20:12:59 +0300

    update with babel

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,11 +10,12 @@ export default ({config}: {config: webpack.Configuration}) => {
 		entry: '',
 		src: path.resolve(__dirname, '..', '..', 'src'),
 	};
-	config.resolve!.modules!.push(paths.src);
-	config.resolve!.extensions!.push('.ts', '.tsx');
+	config!.resolve!.modules!.push(paths.src);
+	config!.resolve!.extensions!.push('.ts', '.tsx');
 
 	// eslint-disable-next-line no-param-reassign
-	config.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
+	// @ts-ignore
+	config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
 		if (/svg/.test(rule.test as string)) {
 			return {...rule, exclude: /\.svg$/i};
 		}
@@ -22,7 +23,7 @@ export default ({config}: {config: webpack.Configuration}) => {
 		return rule;
 	});
 
-	config.module!.rules.push({
+	config!.module!.rules.push({
 		test: /\.svg$/,
 		use: ['@svgr/webpack'],
 	});
@@ -30,7 +31,7 @@ export default ({config}: {config: webpack.Configuration}) => {
 
 	config!.plugins!.push(
 		new DefinePlugin({
-			__IS_DEV__: true,
+			__IS_DEV__: JSON.stringify(true),
 			__API__: JSON.stringify(''),
 			__PROJECT__: JSON.stringify('storybook'),
 		})
