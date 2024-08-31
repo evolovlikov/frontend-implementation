dae1adf6cb854e2629ec9a067949f47be071bd4d
webgeny <evgenyvolovlikov@gmail.com>
2024-09-01 01:58:06 +0300

    rmv tests async dwnld for pages

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import {BuildOptions} from './types/config';
 
 export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -20,6 +21,11 @@ export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): web
 			__API__: JSON.stringify(apiUrl),
 			__PROJECT__: JSON.stringify(project),
 		}),
+		new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
+        }),
 	];
 
 	if (isDev) {
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -9,6 +9,7 @@ export default ({config}: {config: webpack.Configuration}) => {
 		html: '',
 		entry: '',
 		src: path.resolve(__dirname, '..', '..', 'src'),
+		buildLocales: path.resolve(__dirname, 'build', 'locales'),
 	};
 	config!.resolve!.modules!.push(paths.src);
 	config!.resolve!.extensions!.push('.ts', '.tsx');
