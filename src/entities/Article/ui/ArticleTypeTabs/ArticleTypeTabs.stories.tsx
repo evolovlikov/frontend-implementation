4736bd02fc27facd3380354462438e77cc0bc05c
webgeny <evgenyvolovlikov@gmail.com>
2024-09-01 00:58:09 +0300

    add prettier

--- src/entities/Article/ui/ArticleTypeTabs/ArticleTypeTabs.stories.tsx
+++ src/entities/Article/ui/ArticleTypeTabs/ArticleTypeTabs.stories.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import { ArticleTypeTabs } from './ArticleTypeTabs';
+import {ArticleTypeTabs} from './ArticleTypeTabs';
 
 export default {
-    title: 'entities/Article/ArticleTypeTabs',
-    component: ArticleTypeTabs,
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
+	title: 'entities/Article/ArticleTypeTabs',
+	component: ArticleTypeTabs,
+	argTypes: {
+		backgroundColor: {control: 'color'},
+	},
 } as ComponentMeta<typeof ArticleTypeTabs>;
 
 const Template: ComponentStory<typeof ArticleTypeTabs> = (args) => <ArticleTypeTabs {...args} />;
