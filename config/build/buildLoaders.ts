5f4c4488dc57e168d323c2a61b6f11b2887034bb
webgeny <webgeny@mail.ru>
2024-09-08 20:19:05 +0300

    add babel plugins & optimize dev build

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -11,16 +11,18 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 		use: ['@svgr/webpack'],
 	};
 
-	const babelLoader = buildBabelLoader(options);
+	const codeBabelLoader = buildBabelLoader({...options, isTsx: false});
+	const tsxCodeBabelLoader = buildBabelLoader({...options, isTsx: true});
 
 	const cssLoader = buildCssLoader(isDev);
 
 	// Если не используем тайпскрипт - нужен babel-loader
-	const typescriptLoader = {
-		test: /\.tsx?$/,
-		use: 'ts-loader',
-		exclude: /node_modules/,
-	};
+	// const typescriptLoader = {
+	// 	test: /\.tsx?$/,
+	// 	use: 'ts-loader',
+	// 	exclude: /node_modules/,
+	// };
+
 
 	const fileLoader = {
 		test: /\.(png|jpe?g|gif|woff2|woff)$/i,
@@ -31,5 +33,12 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 		],
 	};
 
-	return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
+	return [
+		fileLoader,
+		svgLoader,
+		codeBabelLoader,
+		tsxCodeBabelLoader,
+		// typescriptLoader,
+		cssLoader,
+	];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
 import {BuildOptions} from './types/config';
 
@@ -28,6 +29,15 @@ export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): web
 		new CircularDependencyPlugin({
 			exclude: /node_modules/,
 			failOnError: true,
+		}),
+		new ForkTsCheckerWebpackPlugin({
+			typescript: {
+				diagnosticOptions: {
+					semantic: true,
+					syntactic: true,
+				},
+				mode: 'write-references',
+			},
 		}),
 	];
 
